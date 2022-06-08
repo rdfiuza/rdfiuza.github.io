53e501700f9671b4efcefe20c8efe160c0774df4
rdfiuza <rdfiuza@gmail.com>
2022-06-08 02:18:56 -0300

    jquery importado (utilizando método detach no main)

--- certificados.js
+++ certificados.js
@@ -1,10 +1,14 @@
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-3.6.0.min.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 function clicked(num){
     if(document.getElementById("elementoimagem")){
       let rem = document.getElementById("elementoimagem")
       rem.remove()
     }
     let main = document.getElementById("principal")
-    main.style.visibility = "hidden"
+    
     let body = document.body
     body.style.backgroundColor = "black"
   
@@ -18,4 +22,7 @@ function clicked(num){
   doc.setAttribute("id", "elementoimagem")
   frenteElement.style.visibility = "visible"
   frenteElement.appendChild(img)
+  $(function(){
+    $(main).detach();
+  });
   }
