6b07b573d029415cce37437dabceb567724ff3ee
rdfiuza <rdfiuza@gmail.com>
2022-06-08 13:35:22 -0300

    Atualização na página de certificados

--- certificados.js
+++ certificados.js
@@ -1,28 +1,42 @@
 var script = document.createElement('script');
 script.src = '//code.jquery.com/jquery-3.6.0.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
+let main = document.getElementById("principal")
+let body = document.body
 
 function clicked(num){
     if(document.getElementById("elementoimagem")){
       let rem = document.getElementById("elementoimagem")
       rem.remove()
     }
-    let main = document.getElementById("principal")
-    
-    let body = document.body
+
     body.style.backgroundColor = "black"
   
     let imageCardElement = document.getElementById(num)
     let caminho = imageCardElement.getAttribute("src")
-  const img = new Image(150, 150)
-  img.src = caminho
-  let frenteElement = document.getElementById("frente")
+    const img = new Image(150, 150)
+    img.src = caminho
+    let frenteElement = document.getElementById("frente")
+    
+    let doc = document.body.appendChild(img)
+    doc.setAttribute("id", "elementoimagem")
+    frenteElement.style.visibility = "visible"
+    frenteElement.appendChild(img)
+    $(function(){
+      $(main).detach();
+    });
   
-  let doc = document.body.appendChild(img)
-  doc.setAttribute("id", "elementoimagem")
-  frenteElement.style.visibility = "visible"
-  frenteElement.appendChild(img)
-  $(function(){
-    $(main).detach();
-  });
+}
+function btnClose(){
+  if(document.getElementById("elementoimagem")){
+    let rem = document.getElementById("elementoimagem")
+    rem.remove()
+    $(function(){
+      $(main).appendTo("body");
+    });
   }
+  let frenteElement = document.getElementById("frente")
+  frenteElement.style.visibility = "hidden"
+  
+    body.style.backgroundColor = "white"
+}
