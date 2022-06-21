dfcbc3b6cfa5882466acb49c2fe3669f4f706879
rdfiuza <rdfiuza@gmail.com>
2022-06-21 20:04:18 -0300

    js added to index

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById("html").onclick = function(){
+    window.open("https://developer.mozilla.org/en-US/docs/Web/HTML")
+}
+document.getElementById("css").onclick = function(){
+    window.open("https://developer.mozilla.org/en-US/docs/Web/CSS")
+}
+document.getElementById("javascript").onclick = function(){
+    window.open("https://developer.mozilla.org/en-US/docs/Web/JavaScript")
+}
+document.getElementById("bootstrap").onclick = function(){
+    window.open("https://getbootstrap.com/")
+}
+document.getElementById("jquery").onclick = function(){
+    window.open("https://jquery.com/")
+}
+document.getElementById("git").onclick = function(){
+    window.open("https://git-scm.com/")
+}
+document.getElementById("github").onclick = function(){
+    window.open("https://github.com/")
+}
